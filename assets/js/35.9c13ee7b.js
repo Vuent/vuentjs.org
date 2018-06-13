d6e2cfbd0b9183afe59230a270994ef23186b9ca
Artur Mizera <arturmiz@users.noreply.github.com>
2018-06-13 21:54:12 +0200

    deploy for v0.1.0

---
+++ assets/js/35.9c13ee7b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[35],{112:function(n,t,e){"use strict";e.r(t);var c={name:"CheckboxLabel",components:{VntCheckbox:e(60).b}},o=e(0),s=Object(o.a)(c,function(){var n=this.$createElement,t=this._self._c||n;return t("div",{staticClass:"playground"},[t("vnt-checkbox",[this._v("Option")])],1)},[],!1,null,null,null);t.default=s.exports}}]);
